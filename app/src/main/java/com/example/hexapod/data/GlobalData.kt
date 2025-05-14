5644505061642c4d59529cb070f931d0ea201359
nickolaus125 <mboniecki.w@gmail.com>
2025-05-14 11:40:32 +0200

    added loop for control screen

--- app/src/main/java/com/example/hexapod/data/GlobalData.kt
+++ app/src/main/java/com/example/hexapod/data/GlobalData.kt
@@ -1,28 +1,6 @@
 package com.example.hexapod.data
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-
 object GlobalData {
 
-    var sliderPosition = 0f
-    var sliderPosition2 = 0f
-    var sliderPosition3 = 0f
-    var sliderPosition4 = 0f
-    var sliderPosition5 = 0f
-    var sliderPosition6 = 0f
-    var sliderPosition7 = 0f
-    var sliderPosition8 = 0f
-    var sliderPosition9 = 0f
-    var sliderPosition10 = 0f
-    var sliderPosition11 = 0f
-    var sliderPosition12 = 0f
-    var sliderPosition13 = 0f
-    var sliderPosition14 = 0f
-    var sliderPosition15 = 0f
-    var sliderPosition16  = 0f
-    var sliderPosition17  = 0f
-    var sliderPosition18  = 0f
+    var sliderPosition = FloatArray(18){ 0f }
 }
