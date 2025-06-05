65072b51e4e58c16fc4a4bbf56f08fbef7f80a12
nickolaus125 <mboniecki.w@gmail.com>
2025-06-05 17:55:44 +0200

    Input boxes for points, other minor changes

--- app/src/main/java/com/example/hexapod/data/GlobalData.kt
+++ app/src/main/java/com/example/hexapod/data/GlobalData.kt
@@ -3,4 +3,7 @@ package com.example.hexapod.data
 object GlobalData {
 
     var sliderPosition = FloatArray(18){ 0f }
+
+    var positionX = 0f
+    var positionY = 0f
 }
--- app/src/main/java/com/example/hexapod/ui/MainScreen.kt
+++ app/src/main/java/com/example/hexapod/ui/MainScreen.kt
@@ -7,11 +7,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,6 +50,9 @@ fun MainScreen(){
     val radioOptions = listOf("Bigate", "Ripplegate", "Trigate")
     val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
 
+    var posX1 by remember{mutableStateOf(GlobalData.positionX.toString())}
+    var posY1 by remember { mutableStateOf(GlobalData.positionY.toString()) }
+
     DisposableEffect(Unit) {
         timer.schedule(object : TimerTask() {
             override fun run() {
@@ -59,126 +68,174 @@ fun MainScreen(){
         }
     }
 
-    Column(
+    LazyColumn(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Status: \n",
-            fontSize = 40.sp,
-            lineHeight = 60.sp,
-            textAlign = TextAlign.Center,
-        )
+        item {
+            Text(
+                text = "Status: \n",
+                fontSize = 40.sp,
+                lineHeight = 60.sp,
+                textAlign = TextAlign.Center,
+            )
+        }
 
         val textCon = if (isConnected) "Connected" else "Disconnected"
-        Text(
-            text = textCon,
-            fontSize = 60.sp,
-            lineHeight = 60.sp,
-            textAlign = TextAlign.Center,
-            color = if (isConnected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error
-        )
+        item{
+            Text(
+                text = textCon,
+                fontSize = 60.sp,
+                lineHeight = 60.sp,
+                textAlign = TextAlign.Center,
+                color = if (isConnected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error
+            )
 
-        Image(
-            painter = painterResource(R.drawable.hexapod),
-            contentDescription = "Icon"
-        )
+            Image(
+                painter = painterResource(R.drawable.hexapod),
+                contentDescription = "Icon"
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+        }
 
-        Spacer(modifier = Modifier.height(64.dp))
-
-        Row {
-            Button(
-                onClick = {
-                    if (selectedOption == "Bigate"){
-                        WebSocketHandler.sendMessage("bigate11")
-                    }
-                    else if (selectedOption == "Ripplegate"){
-                        WebSocketHandler.sendMessage("ripplegate11")
-                    }
-                    else if (selectedOption == "Trigate"){
-                        WebSocketHandler.sendMessage("trigate11")
-                    }
-                          },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button11), fontSize = 12.sp)
+        item {
+            Row {
+                Button(
+                    onClick = {
+                        if (selectedOption == "Bigate") {
+                            WebSocketHandler.sendMessage("bigate11")
+                        } else if (selectedOption == "Ripplegate") {
+                            WebSocketHandler.sendMessage("ripplegate11")
+                        } else if (selectedOption == "Trigate") {
+                            WebSocketHandler.sendMessage("trigate11")
+                        }
+                    },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button11), fontSize = 12.sp)
+                }
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button12), fontSize = 12.sp)
+                }
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button13), fontSize = 12.sp)
+                }
             }
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button12), fontSize = 12.sp)
+        }
+        item {
+            Row {
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button21), fontSize = 12.sp)
+                }
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button22), fontSize = 12.sp)
+                }
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button23), fontSize = 12.sp)
+                }
             }
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button13), fontSize = 12.sp)
+        }
+        item {
+            Row {
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button31), fontSize = 12.sp)
+                }
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button32), fontSize = 12.sp)
+                }
+                Button(
+                    onClick = { WebSocketHandler.sendMessage(" ") },
+                    modifier = Modifier.padding(12.dp).weight(1f)
+                ) {
+                    Text(text = stringResource(R.string.button33), fontSize = 12.sp)
+                }
+            }
+        }
+        item {
+            Row(Modifier.selectableGroup()) {
+                Column(
+                    modifier = Modifier.padding(12.dp).weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    RadioButton(
+                        selected = ("Bigate" == selectedOption),
+                        onClick = { onOptionSelected("Bigate") })
+                    Text(text = "Bigate")
+                }
+                Column(
+                    modifier = Modifier.padding(12.dp).weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    RadioButton(
+                        selected = ("Ripplegate" == selectedOption),
+                        onClick = { onOptionSelected("Ripplegate") })
+                    Text(text = "Ripplegate")
+                }
+                Column(
+                    modifier = Modifier.padding(12.dp).weight(1f),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    RadioButton(
+                        selected = ("Trigate" == selectedOption),
+                        onClick = { onOptionSelected("Trigate") })
+                    Text(text = "Trigate")
+                }
+
             }
         }
 
-        Row {
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button21), fontSize = 12.sp)
+        item{
+            Row(){
+                OutlinedTextField(
+                    value = posX1,
+                    onValueChange = {
+                        posX1 = it
+                        val check1 = it.toFloatOrNull()
+                        if (check1 != null){
+                            GlobalData.positionX = it.toFloat()
+                        }
+                    },
+                    label = { Text("Position X") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+                    modifier = Modifier
+                        .padding(16.dp).weight(1f),
+                )
+                OutlinedTextField(
+                    value = posY1,
+                    onValueChange = {
+                        posY1 = it
+                        val check2 = it.toFloatOrNull()
+                        if(check2 != null){
+                            GlobalData.positionY = it.toFloat()
+                        }
+                    },
+                    label = {Text("Position Y")},
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+                    modifier = Modifier
+                            .padding(16.dp).weight(1f),
+                )
             }
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button22), fontSize = 12.sp)
-            }
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button23), fontSize = 12.sp)
-            }
-        }
-
-        Row {
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button31), fontSize = 12.sp)
-            }
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button32), fontSize = 12.sp)
-            }
-            Button(
-                onClick = { WebSocketHandler.sendMessage(" ") },
-                modifier = Modifier.padding(12.dp).weight(1f)
-            ) {
-                Text(text = stringResource(R.string.button33), fontSize = 12.sp)
-            }
-        }
-
-        Row(Modifier.selectableGroup()){
-            Column(
-                modifier = Modifier.padding(12.dp).weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                RadioButton(selected = ("Bigate" == selectedOption) ,onClick = {onOptionSelected("Bigate")})
-                Text(text = "Bigate")
-            }
-            Column(
-                modifier = Modifier.padding(12.dp).weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                RadioButton(selected = ("Ripplegate" == selectedOption) ,onClick = {onOptionSelected("Ripplegate")})
-                Text(text = "Ripplegate")
-            }
-            Column(
-                modifier = Modifier.padding(12.dp).weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally) {
-                RadioButton(selected = ("Trigate" == selectedOption) ,onClick = {onOptionSelected("Trigate")})
-                Text(text = "Trigate")
-            }
-
         }
     }
 }
